370c393e6a06bbbcb2a86c1c70c43ab5c1f934ad
m1sha <m1hailostertag@gmail.com>
2024-06-08 01:09:44 +0700

    Surface

--- src/surface.ts
+++ src/surface.ts
@@ -1,32 +1,58 @@
 import { Rect } from "./rect"
 
 export class Surface {
-  private _canvas: HTMLCanvasElement | OffscreenCanvas
-  private ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D
-  private _rect: Rect
+  #canvas: HTMLCanvasElement | OffscreenCanvas
+  #ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D
+  #rect: Rect
   
   constructor(width: number, height: number, useAlpha: boolean = true, virtual: boolean = false) {
-    this._canvas = virtual ? new OffscreenCanvas(width, height) : document.createElement('canvas')
-    this._canvas.width = width
-    this._canvas.height = height
-    this._rect = new Rect(0, 0, width, height)
-    this.ctx = this._canvas.getContext('2d', { alpha: useAlpha })! as CanvasRenderingContext2D
+    this.#canvas = virtual ? new OffscreenCanvas(width, height) : document.createElement('canvas')
+    this.#canvas.width = width
+    this.#canvas.height = height
+    this.#rect = new Rect(0, 0, width, height)
+    this.#ctx = this.#canvas.getContext('2d', { alpha: useAlpha })! as CanvasRenderingContext2D
   }
 
-  get draw () { return this.ctx }
+  get draw () { return this.#ctx }
 
-  get rect () { return this._rect }
+  get rect () { return this.#rect }
 
-  clear (color: string = "") {
-    this.ctx.clearRect(0, 0, this._canvas.width, this._canvas.height)
+  get width () { return this.#rect.width }
+
+  get height () { return this.#rect.height }
+
+  clear () {
+    this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height)
+  }
+
+  fill (color: string) {
+    this.#ctx.fillStyle = color
+    this.#ctx.fillRect(0, 0, this.width, this.height)
   }
 
   blit(surface: Surface, rect: Rect) {
-    this.ctx.drawImage(surface._canvas, rect.x, rect.y, surface._canvas.width, surface._canvas.height)
+    this.#ctx.drawImage(surface.#canvas, rect.x, rect.y, surface.width, surface.height)
   }
 
+  zoom (index: number) {
+    this.#ctx.drawImage(this.#canvas, 0, 0, this.width, this.height, 0, 0, this.width * index, this.height * index)
+  }
+
+  flip (position: 'x' | 'y' | 'xy') {
+    const w = position === 'y' ? 0 : this.width
+    const h = position === 'x' ? 0 : this.height
+    const x = position === 'y' ? 1 : -1
+    const y = position === 'x' ? 1 : -1
+    
+    this.#ctx.translate(w, h)
+    this.#ctx.scale(x, y)
+    this.#ctx.drawImage(this.#canvas, 0, 0, this.width, this.height)
+  }
+
+  /*
+
   blit_i(surface: Surface, rect: Rect) {
-    this.ctx.drawImage(surface.rasterizate()!, rect.x, rect.y, surface._canvas.width, surface._canvas.height)
+    this.ctx.drawImage(surface.rasterizate()!, rect.x, rect.y, surface.width, surface.height)
   }
 
   rasterizate () {
@@ -36,9 +62,11 @@ export class Surface {
     return img
   }
 
+  */
+
   static fromImage(image: HTMLImageElement, rect: Rect, useAlpha: boolean = true) {
     const surface = new Surface(rect.width, rect.height, useAlpha)
-    surface.ctx.drawImage(image, rect.x, rect.y, rect.width, rect.height)
+    surface.#ctx.drawImage(image, rect.x, rect.y, rect.width, rect.height)
     return surface
   }
 }
