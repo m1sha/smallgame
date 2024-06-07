6c183f38e51d743253b58fc51b2cf83dd854a148
m1sha <m1hailostertag@gmail.com>
2024-06-08 01:10:19 +0700

    loadTileMap

--- src/assets.ts
+++ src/assets.ts
@@ -1,14 +1,18 @@
+import { TileMap } from "./tile-map"
 import { Rect } from "./rect"
 import { Surface } from "./surface"
 
-export class Assets {
-  async loadImage (url: string): Promise<Surface> {
-    const [img, rect] = await loadImage(url)
-    return Surface.fromImage(img, rect)
-  }
+export async function loadImage (url: string): Promise<Surface> {
+  const [img, rect] = await __loadImage(url)
+  return Surface.fromImage(img, rect)
 }
 
-function loadImage (url: string): Promise<[HTMLImageElement, Rect]> {
+export async function loadTileMap(tileWidth: number, titleHight: number, url: string): Promise<TileMap> {
+  const [img, rect] = await __loadImage(url)
+  return TileMap.fromImage(tileWidth, titleHight, img, rect)
+}
+
+function __loadImage (url: string): Promise<[HTMLImageElement, Rect]> {
   return new Promise((resolve, reject) => {
     const img = new Image()
     img.src = url
@@ -20,5 +24,4 @@ function loadImage (url: string): Promise<[HTMLImageElement, Rect]> {
     }
     img.onerror = e => reject(e)
   })
-  
 }
