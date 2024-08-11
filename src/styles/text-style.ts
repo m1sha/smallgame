9e13a9e843c1c16d368fb819243bb92ee8f5fba8
m1sha <m1hailostertag@gmail.com>
2024-08-11 15:31:26 +0700

    text

---
+++ src/styles/text-style.ts
@@ -0,0 +1,44 @@
+import { FillStrokeOrder } from "./fill-stroke-order"
+
+export type FontWeight = 'normal' | 'bold' | 'lighter' | 'bolder' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
+export type FontVariant = 'normal' | 'small-caps'
+export interface TTextStyle {
+  color?: string
+  outlineColor?: string
+  outlineWidth?: number
+  fontName?: string
+  fontSize?: string
+  bold? : boolean | FontWeight
+  italic?: boolean
+  fontVariant?: FontVariant
+  fillStrokeOrder?: FillStrokeOrder
+  // readonly shadow?: Shadow
+}
+
+export class TextStyle {
+  color: string
+  outlineColor: string
+  outlineWidth: number
+  fontName: string
+  fontSize: string
+  bold : boolean | FontWeight
+  italic: boolean
+  fontVariant: FontVariant
+  fillStrokeOrder: FillStrokeOrder
+  // #shadow?: Shadow
+
+  constructor (style: TTextStyle) {
+    
+    this.color = style.color ? style.color : '#212121'
+    this.outlineColor = style.outlineColor ? style.outlineColor : 'transparent'
+    this.outlineWidth = style.outlineWidth ? style.outlineWidth : 1
+    this.fontName = style.fontName ? style.fontName : 'serif'
+    this.fontSize = style.fontSize ? style.fontSize : '12px'
+    this.bold = style.bold ? style.bold : 'normal'
+    this.italic = style.italic ? style.italic : false
+    this.fontVariant = style.fontVariant ? style.fontVariant : 'normal'
+    this.fillStrokeOrder = style.fillStrokeOrder ? style.fillStrokeOrder : 'fill-first'
+    //this.#shadow = style.shadow
+    
+  }
+}
---
+++ src/text.ts
@@ -0,0 +1,55 @@
+import { Surface } from "./surface"
+import { Drawable } from "./drawable"
+import { TPoint } from "./point"
+import { TTextStyle, TextStyle } from "./styles/text-style"
+
+export class Text implements Drawable {
+  content: string
+  pos: TPoint
+  style: TextStyle
+
+  constructor (content: string, pos: TPoint, style: TTextStyle | TextStyle) {
+    this.content = content
+    this.pos = pos
+    this.style = style instanceof TextStyle ? style : new TextStyle(style)
+  }
+  
+  draw (suface: Surface): void {
+    assignTextStyle(suface.draw as any, this.style)
+    drawText(suface.draw as any, this.style, this.content, this.pos.x, this.pos.y)
+  }
+}
+
+
+function assignTextStyle (ctx: CanvasRenderingContext2D, style: TextStyle) {
+  style = style || {}
+  ctx.fillStyle = style.color
+  ctx.strokeStyle = style.outlineColor
+  if (style.outlineWidth) ctx.lineWidth = style.outlineWidth
+  const fontName = style.fontName || 'serif'
+  const fontSize = style.fontSize || '10pt'
+  const bold = typeof style.bold === 'boolean' && style.bold ? 'bold ' : style.bold ? style.bold : 'normal'
+  const italic = style.italic ? 'italic ' : 'normal'
+  const fontVariant = style.fontVariant ?? 'normal'
+  ctx.font = `${italic} ${bold} ${fontVariant} ${fontSize} ${fontName}`
+}
+
+function drawText (ctx: CanvasRenderingContext2D, style: TextStyle, text: string, x: number, y: number) {
+  if (style.fillStrokeOrder === 'stroke-first') {
+    strokeText(ctx, style, text, x, y)
+    fillText(ctx, style, text, x, y)
+  }
+
+  if (!style.fillStrokeOrder || style.fillStrokeOrder === 'fill-first') {
+    fillText(ctx, style, text, x, y)
+    strokeText(ctx, style, text, x, y)
+  }
+}
+
+function fillText (ctx: CanvasRenderingContext2D, style: TextStyle, text: string, x: number, y: number) {
+  if (style.color !== 'transparent') ctx.fillText(text, x, y)
+}
+
+function strokeText (ctx: CanvasRenderingContext2D, style: TextStyle, text: string, x: number, y: number) {
+  if (style.outlineColor && style.outlineColor !== 'transparent') ctx.strokeText(text, x, y)
+}
