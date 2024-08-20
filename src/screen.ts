c65c0e449a949b24a7fbabd69d215bed5b7748ae
m1sha <m1hailostertag@gmail.com>
2024-08-20 17:13:52 +0700

    Screen. relation suface size and canvas size

--- src/screen.ts
+++ src/screen.ts
@@ -1,5 +1,9 @@
 import { Surface } from './surface'
 
 export class Screen extends Surface {
-  
+  get delta () {
+    const canvas = this.canvas as HTMLCanvasElement
+    const rect = canvas.getBoundingClientRect()
+    return { x: rect.width / canvas.width, y: rect.height / canvas.height }
+  }
 }
