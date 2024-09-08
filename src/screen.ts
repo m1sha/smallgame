c516f40db0ba2390f139bc0ac59d27d5cb259ddb
m1sha <m1hailostertag@gmail.com>
2024-09-09 00:21:25 +0700

    Screen cursor

--- src/screen.ts
+++ src/screen.ts
@@ -2,8 +2,20 @@ import { Surface } from './surface'
 
 export class Screen extends Surface {
   get delta () {
-    const canvas = this.canvas as HTMLCanvasElement
+    const canvas = this.innteralCanvas
     const rect = canvas.getBoundingClientRect()
     return { x: rect.width / canvas.width, y: rect.height / canvas.height }
+  }
+
+  get cursor () {
+    return this.innteralCanvas.style.cursor
+  }
+
+  set cursor (value: string) {
+    this.innteralCanvas.style.cursor = value
+  }
+
+  private get innteralCanvas() {
+    return this.canvas as HTMLCanvasElement
   }
 }
