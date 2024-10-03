5932d922467f8594a766bfce90a59572577d15e3
m1sha <m1hailostertag@gmail.com>
2024-10-04 01:02:05 +0700

    lerp for scalar

--- src/utils/lerp.ts
+++ src/utils/lerp.ts
@@ -1,6 +1,6 @@
 import { TPoint } from "../point"
 
-const _lerp = (g: number, c: number, dt: number): number => {
+const lerpScalar = (g: number, c: number, dt: number): number => {
   const d = g - c
   if (d > dt) return c + dt
   if (d < -dt) return c - dt
@@ -8,8 +8,8 @@ const _lerp = (g: number, c: number, dt: number): number => {
 }
 
 const lerp = (g: TPoint, c: TPoint, dt: number): void => {
-  c.x = _lerp(g.x, c.x, dt)
-  c.y = _lerp(g.y, c.y, dt)
+  c.x = lerpScalar(g.x, c.x, dt)
+  c.y = lerpScalar(g.y, c.y, dt)
 }
 
-export { lerp }
+export { lerp, lerpScalar }
