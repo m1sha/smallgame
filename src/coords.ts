531f48bdc51c77ab9d6f5c69e52fd53541963426
m1sha <m1hailostertag@gmail.com>
2024-10-04 01:00:12 +0700

    cartesian coordinate system

--- src/coords.ts
+++ src/coords.ts
@@ -1,6 +1,6 @@
 import { setPoint, TPoint } from "./point"
 
-export type CoordinateSystem = 'screen' | 'math'
+export type CoordinateSystem = 'screen' | 'math' | 'cartesian'
 
 
 const fromMathCoord = (x: number, y: number, half_width: number, half_height: number) => (
@@ -11,11 +11,16 @@ const fromScreenCoord = (x: number, y: number, half_width: number, half_height: 
   setPoint(x / half_width - 1,  y / half_height - 1)
 )
 
+const fromCartesianCoord = (x: number, y: number, half_width: number, half_height: number) => (
+  setPoint(x + half_width, half_height - y)
+)
+
 const coordconv = (sys: CoordinateSystem, point: TPoint, width: number, height: number): TPoint => {
   switch (sys) {
     case "screen": return point
     case "math": return fromMathCoord(point.x, point.y, width / 2, height / 2)
+    case "cartesian": return fromCartesianCoord(point.x, point.y, width / 2, height / 2)
   }
 }
 
-export { fromMathCoord, fromScreenCoord, coordconv }
+export { fromMathCoord, fromScreenCoord, fromCartesianCoord, coordconv }
