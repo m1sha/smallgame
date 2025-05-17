25097c81238255fa73329e71bfa8f6ce775ced7e
m1sha <m1hailostertag@gmail.com>
2025-05-17 22:31:48 +0700

    Point overloads

--- src/point.ts
+++ src/point.ts
@@ -12,48 +12,111 @@ export class Point {
     if (typeof args[0] === 'number' && typeof args[1] === 'number' ) {
       this._x = args[0]
       this._y = args[1]
+      return
     } 
     else if (typeof args[0] === 'object' && Object.hasOwn(args[0], 'x') && Object.hasOwn(args[0], 'y') ) {
       this._x = args[0].x
       this._y = args[0].y
+      return
     }
+
+    throw new Error('unsupported arguments.')
   }
 
-  get x () { return this._x }
+  get x (): number { return this._x }
   
   set x (value: number) { this._x = value }
   
-  get y () { return this._y }
+  get y (): number { return this._y }
   
   set y (value: number) { this._y = value }
 
-  move (x: number, y: number) {
-    return new Point(x, y)
+  move (value: number): Point 
+  move (point: TPoint): Point 
+  move (x: number, y: number): Point
+  move (...args: Array<any>): Point {
+    if (args.length === 1 && typeof args[0] === 'number') {
+      return new Point(args[0], args[0])  
+    }
+
+    if (typeof args[0] === 'number' && typeof args[1] === 'number') {
+      return new Point(args[0], args[1]) 
+    }
+
+    if (typeof args[0] === 'object' && args[0] && typeof args[0].x === 'number' && typeof args[0].y === 'number') {
+      return new Point(args[0].x, args[0].y)
+    }
+
+    throw new Error('unsupported arguments.')
   }
 
-  moveSelf (x: number, y: number) {
+  moveSelf (value: number): Point 
+  moveSelf (point: TPoint): Point 
+  moveSelf (x: number, y: number): Point
+  moveSelf  (...args: Array<any>): Point {
+    if (args.length === 1 && typeof args[0] === 'number') {
+      this.x = args[0]
+      this.y = args[0]
+      return this
+    }
+
+    if (typeof args[0] === 'number' && typeof args[1] === 'number') {
+      this.x = args[0]
+      this.y = args[1]
+      return this
+    }
+
+    if (typeof args[0] === 'object' && args[0] && typeof args[0].x === 'number' && typeof args[0].y === 'number') {
+      this.x = args[0].x
+      this.y = args[0].y
+      return this
+    }
+
+    throw new Error('unsupported arguments.')
+  }
+
+  moveX (x: number): Point {
+    return new Point(x, this.y)
+  }
+
+  moveY (y: number): Point {
+    return new Point(this.x, y)
+  }
+
+  moveXSelf (x: number): Point {
     this.x = x
+    return this
+  }
+
+  moveYSelf (y: number): Point {
     this.y = y
     return this
   }
 
-  moveX (x: number) {
-    return new Point(x, this.y)
-  }
+  shift (d: number): Point 
+  shift (dp: TPoint): Point 
+  shift (dx: number, dy: number): Point
+  shift (...args: Array<any>): Point {
+    if (args.length === 1 && typeof args[0] === 'number' ) {
+      return new Point(this.x + args[0], this.y + args[0])
+    }
 
-  moveY (y: number) {
-    return new Point(this.x, y)
-  }
+    if (typeof args[0] === 'number' && typeof args[1] === 'number') {
+      return new Point(this.x + args[0], this.y + args[1])
+    }
 
-  shift (dx: number, dy: number) {
-    return new Point(this.x + dx, this.y + dy)
+    if (typeof args[0] === 'object' && args[0] && typeof args[0].x === 'number' && typeof args[0].y === 'number') {
+      return new Point(this.x + args[0].x, this.y + args[0].y)
+    }
+
+    throw new Error('unsupported arguments.')
   }
 
   shiftSelf (d: number): Point 
   shiftSelf (dp: TPoint): Point 
   shiftSelf (dx: number, dy: number): Point 
   shiftSelf (...args: Array<any>): Point {
-    if (args.length === 1 && typeof args[0] === 'number' ){
+    if (args.length === 1 && typeof args[0] === 'number' ) {
       this.x += args[0]
       this.y += args[0]
       return this
@@ -74,12 +137,22 @@ export class Point {
     throw new Error('unsupported arguments.')
   }
 
-  shiftX (dx: number) {
+  shiftX (dx: number): Point {
     return new Point(this.x + dx, this.y)
   }
 
-  shiftY (dy: number) {
+  shiftY (dy: number): Point {
     return new Point(this.x, this.y + dy)
+  }
+
+  shiftXSelf (dx: number): Point {
+    this.x += dx
+    return this
+  }
+
+  shiftYSelf (dy: number): Point {
+    this.y += dy
+    return this
   }
 
   scale (d: number): Point 
@@ -127,38 +200,83 @@ export class Point {
     throw new Error('unsupported arguments.')
   }
 
-  scaleX(dx: number) {
+  scaleX (dx: number): Point {
     return new Point(this.x * dx, this.y)
   }
 
-  scaleY(dy: number) {
+  scaleY (dy: number): Point {
     return new Point(this.x, this.y * dy)
   }
 
-  rotate (deg: number) {
+  scaleXSelft (dx: number): Point {
+    this.x *= dx
+    return this
+  }
+
+  scaleYSelft (dy: number): Point {
+    this.y *= dy
+    return this
+  }
+
+  rotate (deg: number): Point {
     const a = rad(deg)
     return new Point(this.x + Math.cos(a), this.y + Math.sin(a))
   }
 
-  rotateSelf (deg: number) {
+  rotateSelf (deg: number): Point {
     const a = rad(deg)
     this.shiftSelf(Math.cos(a), Math.sin(a))
     return this
   }
 
-  negative () {
+  neg (): Point {
     return new Point(-this.x, -this.y)
   }
 
-  negativeSelf () {
+  negX (): Point {
+    return new Point(-this.x, this.y)
+  }
+
+  negY (): Point {
+    return new Point(this.x, -this.y)
+  }
+
+  negSelf (): Point {
     this.x *= -1
     this.y *= -1
     return this
   }
 
-  static get zero () { return new Point(0, 0) }
+  negXSelf (): Point {
+    this.x *= -1
+    return this
+  }
+
+  negYSelf (): Point {
+    this.y *= -1
+    return this
+  }
+
+  negative (): Point { return this.neg() }
+  negativeX (): Point { return this.negX() }
+  negativeY (): Point { return this.negY() }
+  negativeSelf (): Point { return this.negSelf() }
+  negativeXSelf (): Point { return this.negXSelf() }
+  negativeYSelf (): Point { return this.negYSelf() }
+
+  abs (): Point {
+    return new Point(Math.abs(this.x), Math.abs(this.y))
+  }
+
+  absSelf (): Point {
+    this.x = Math.abs(this.x) 
+    this.y = Math.abs(this.y)
+    return this
+  }
+
+  static get zero (): Point { return new Point(0, 0) }
   
-  static get one () { return new Point(1, 1) }
+  static get one (): Point { return new Point(1, 1) }
 
   static distance (p0: TPoint, p1: TPoint): number {
     const dx = Math.pow(p1.x - p0.x, 2)
