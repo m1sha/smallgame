a7e59d579f6480feb707d4cba5bec94a082c63e6
m1sha <m1hailostertag@gmail.com>
2025-07-12 14:37:05 +0700

    gl texture change texture setting

--- src/gl/gl-texture.ts
+++ src/gl/gl-texture.ts
@@ -25,14 +25,20 @@ export class GlTexture {
     //gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true)
     gl.activeTexture(TextureSlots[this.#slot]) // options.slot
     gl.bindTexture(gl.TEXTURE_2D, texture)
+    //gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RGBA8, surface.width, surface.height)
     //gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST)
+    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR_MIPMAP_NEAREST)
     // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT)
     if (surface instanceof Surface)
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, surface.draw.origin.canvas)
     else
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, surface.origin)
+
+    gl.generateMipmap(gl.TEXTURE_2D)
   }
 
   update (surface:  Surface | GlSurface) {
