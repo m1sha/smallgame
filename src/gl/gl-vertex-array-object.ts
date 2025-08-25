7ee898f4e5c7924b038022c60e17935ec6f0fcef
m1sha <m1hailostertag@gmail.com>
2025-08-25 22:47:14 +0700

    gl vao

---
+++ src/gl/gl-vertex-array-object.ts
@@ -0,0 +1,22 @@
+import { GlVertexBufferObject } from "./gl-vertex-buffer-object";
+
+export class GlVertexArrayObject {
+  constructor (private ctx: WebGL2RenderingContext, private vaoId: WebGLVertexArrayObject, private vbo: GlVertexBufferObject) {
+    
+  }
+
+  get vertexCount () { return this.vbo.count }
+
+  bind () {
+    this.ctx.bindVertexArray(this.vaoId)
+  }
+
+  unbind () {
+    this.ctx.bindVertexArray(null)
+  }
+
+  remove () {
+    this.vbo.remove()
+    this.ctx.deleteVertexArray(this.vaoId)
+  }
+}
--- src/gl/gl.ts
+++ src/gl/gl.ts
@@ -5,9 +5,10 @@ import { DrawType, getGlType, getGlTypeSize, getShape, GlAttributeTypeMap, GlUni
 import { getVertexAttribPointerTemplate } from "./utils"
 import { GlVertexBufferObject } from "./gl-vertex-buffer-object"
 import { ISurface } from "../interfaces"
-import { GlTextureList, ITextureOptions } from "./gl-texture"
+import { GlTextureList, ITextureOptions } from "./textures"
 import { GlBufferSubData } from "./gl-sub-buffer-data"
 import { GlBuffer } from "./gl-buffer"
+import { GlVertexArrayObject } from "./gl-vertex-array-object"
 
 export class GL {
   #prog: GlProgram | undefined = undefined
@@ -27,9 +28,9 @@ export class GL {
     return Boolean(this.ctx) && Boolean(this.#prog)
   }
 
-  clear () {
+  clear (rgba: number) {
     const gl = this.ctx
-    gl.clearColor(0.0, 0.0, 0.0, 0.0)
+    gl.clearColor((rgba >> 24) & 0xff, (rgba >> 16) & 0xff, (rgba >> 8) & 0xff, rgba & 0xff)
     gl.clear(gl.COLOR_BUFFER_BIT)
     //gl.colorMask(true, true, true, false)
   }
@@ -104,9 +105,24 @@ export class GL {
     return new GlVertexBufferObject(buffer, template, drawType)
   }
 
+  vao<T extends {}> (drawType: DrawType, type: 'float' | 'short' | 'byte' | 'ushort' | 'ubyte', scheme: T, ...arr: Array<number[]>) {
+    const gl = this.ctx
+    const vaoId = gl.createVertexArray()
+    gl.bindVertexArray(vaoId)
+    const vbo = this.vbo(drawType, type, scheme)
+    vbo.push(...arr)
+    const vao = new GlVertexArrayObject(gl, vaoId, vbo)
+    gl.bindVertexArray(null)
+    return vao
+  }
   
   createTexture (samplerName: string, surface: ISurface, options?: ITextureOptions) {
     const sampler = this.uniform(samplerName, 'int')
-    return this.#textures.add(sampler, surface, options ?? { minMag: 'linear' })
+    return this.#textures.addTexture2D(sampler, surface, options ?? { minMag: 'linear' })
+  }
+
+  createTextureArray (samplerName: string, surfaces: ISurface[], options?: ITextureOptions) {
+    const sampler = this.uniform(samplerName, 'int')
+    return this.#textures.addTextureArray(sampler, surfaces, options ?? { minMag: 'linear' })
   }
 }
