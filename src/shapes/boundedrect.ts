0bc5a26cfbe670af8d327eac70334a8b3c73570b
m1sha <m1hailostertag@gmail.com>
2025-10-20 00:48:03 +0700

    Boundedrect added new shapes

--- src/shapes/boundedrect.ts
+++ src/shapes/boundedrect.ts
@@ -5,6 +5,9 @@ import { type RoundedRectangle } from "./roundedrect"
 import { type Circle } from "./circle"
 import { type Line } from "./line"
 import { setPoint } from "../point"
+import { Polygon } from "./polygon"
+import { Arrows } from "./arrows"
+import { Polydots } from "./polydots"
 
 export class Boundedrect {
   static getShapesBounds (shapes: Shape[]): Rect { 
@@ -23,17 +26,32 @@ export class Boundedrect {
       case 'rectangle': return this.getRectangle(shape)
       case 'circle': return this.getCircle(shape)
       case 'line': return this.getLine(shape)
+      case 'polygon': return this.getPolygon(shape)
+      case 'polydots': return this.getPolygon(shape)
+      case 'arrows': return this.getArrows(shape)
 
       default: throw new Error(shape.type + ' type is unsupported')
     }
   }
 
-  private static getRectangle(rect: Rectangle | RoundedRectangle): TRect {
+  private static getArrows (shape: Arrows): TRect  {
+    return setRect(0, 0, 0, 0)
+  }
+
+  private static getPolygon (shape: Polygon | Polydots): TRect  {
+    const ax = shape.points.map(p => p.x)
+    const ay = shape.points.map(p => p.y)
+    const x = Math.min.apply(null, ax)
+    const y = Math.min.apply(null, ay)
+    return setRect(x, y, Math.max.apply(null, ax) - x, Math.max.apply(null, ay) - y)
+  }
+
+  private static getRectangle (rect: Rectangle | RoundedRectangle): TRect {
     const border = rect.style.lineWidth
     return Rect.from(rect).outline(-border)
   }
 
-  private static getCircle({ x, y, radius, style }: Circle): TRect {
+  private static getCircle ({ x, y, radius, style }: Circle): TRect {
     const border = style.lineWidth
     return setRect(x - radius, y - radius, radius * 2 + border, radius * 2 + border)
   }
