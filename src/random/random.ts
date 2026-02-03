21d7509806d3076444982dad4120a36d80dd22ab
m1sha <m1sha@live.ru>
2026-02-03 23:37:55 +0700

    Random (betweeni)

--- src/random/random.ts
+++ src/random/random.ts
@@ -3,7 +3,8 @@ import { Rect, TRect } from "../rect"
 import { uniformRandomScatter } from "./uniform-random-scatter"
 
 const Random = {
-  uniformRandomScatter: <T extends { rect: Rect }>(fieldSize: TSize | TRect, objecs: T[]): void => uniformRandomScatter(fieldSize, objecs)
+  uniformRandomScatter: <T extends { rect: Rect }>(fieldSize: TSize | TRect, objecs: T[]): void => uniformRandomScatter(fieldSize, objecs),
+  betweeni: (min: number, max: number) => (0 | (Math.random() * max)) + min
 }
 
 export { Random }
