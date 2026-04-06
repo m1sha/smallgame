1a9abe93c23f1d1675dccad104357cf59c1f1bc6
m1sha <m1sha@live.ru>
2026-04-06 21:20:26 +0700

    drawRoundedRectangle fixed figure

--- src/sketch/drawers/draw-rounded-rectangle.ts
+++ src/sketch/drawers/draw-rounded-rectangle.ts
@@ -5,6 +5,8 @@ import { drawShape } from "./draw-shape"
 
 export function drawRoundedRectangle (shape: RoundedRectangle, surface: Surface, shift: TPoint, scale: TPoint) {
   const radii = [shape.topLeft, shape.topRight, shape.bottomRight, shape.bottomLeft].filter(p => p)
+  surface.draw.beginPath()
   surface.draw.roundRect(shift.x + shape.x * scale.x, shift.y + shape.y * scale.y, shape.width * scale.x, shape.height * scale.y, radii)
+  surface.draw.closePath()
   drawShape(shape, surface)
 }
